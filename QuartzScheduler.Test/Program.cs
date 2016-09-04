7a64663fb772a094259ed7fe3cd3ba94f9414356
Cihan YEŞİLTAŞ <cihanyesiltas@gmail.com>
2016-09-04 12:50:03 +0300

    QuartzScheduler.Test

---
+++ QuartzScheduler.Test/Program.cs
@@ -0,0 +1,44 @@
+﻿using QuartzScheduler.Infrastructure.Interfaces;
+using QuartzScheduler.Infrastructure.Models;
+using QuartzScheduler.Infrastructure.Utils;
+
+namespace QuartzScheduler.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conn = new Connection
+            {
+                Port = 555,
+                SchedulerName = "QuartzScheduler",
+                ServerName = "localhost"
+            };
+
+            IJobScheduler jobScheduler = JobSchedulerFactory.CreateJobScheduler(conn);
+            var metadata = jobScheduler.GetMetaData();
+            jobScheduler.ScheduleJob("LongJob", "LongJob", "0 0/1 * 1/1 * ? *");
+            jobScheduler.DeleteJob("LongJob", "LongJob");
+
+            var type = JobUtil.Create("QuartzScheduler.Test.LongTimeJob", "QuartzScheduler.Test.LongTimeJob.LongJob");
+
+            jobScheduler.PauseJob("SampleJob", "SampleJob");
+            jobScheduler.UnscheduleJob("SampleJob", "SampleJob");
+            jobScheduler.ScheduleJob("SampleJob", "SampleJob", "0/10 * * * * ?");
+
+            //jobScheduler.AddJob("SampleJob", "SampleJob", "0 0/1 * 1/1 * ? *", type, true, jobData);
+
+            //jobScheduler.UnscheduleJob("SampleJob", "SampleJob");
+            //jobScheduler.ScheduleJob("SampleJob", "SampleJob", "10 * * * * ?");
+
+            //jobScheduler.PauseTrigger("SampleJob", "SampleJob");
+            //jobScheduler.ResumeTrigger("SampleJob", "SampleJob");
+
+            //jobScheduler.Shutdown(true);
+
+            //jobScheduler.TriggerJob("SampleJob", "SampleJob");
+
+            var list = jobScheduler.GetAllJobs();
+        }
+    }
+}
